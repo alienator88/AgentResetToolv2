ba5e7fbb0f265d11886226ef2780203f85efabcc
Alin Lupascu <lupascu.marius@gmail.com>
2020-05-04 13:36:02 -0600

    Add files via upload

---
+++ Equity Toolbox/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Equity_Toolbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.labelInfo1 = new System.Windows.Forms.Label();
+            this.labelInfo2 = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.textBoxBrittniPassword = new System.Windows.Forms.TextBox();
+            this.labelInfo3 = new System.Windows.Forms.Label();
+            this.progressBarStatus = new System.Windows.Forms.ProgressBar();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Location = new System.Drawing.Point(619, 68);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(129, 50);
+            this.buttonGo.TabIndex = 0;
+            this.buttonGo.Text = "Start";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(267, 9);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(257, 29);
+            this.labelTitle.TabIndex = 1;
+            this.labelTitle.Text = "Agent Password Reset";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxName.Location = new System.Drawing.Point(229, 68);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(384, 30);
+            this.textBoxName.TabIndex = 2;
+            // 
+            // labelInfo1
+            // 
+            this.labelInfo1.AutoSize = true;
+            this.labelInfo1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelInfo1.Location = new System.Drawing.Point(34, 68);
+            this.labelInfo1.Name = "labelInfo1";
+            this.labelInfo1.Size = new System.Drawing.Size(172, 25);
+            this.labelInfo1.TabIndex = 3;
+            this.labelInfo1.Text = "Enter agent name:";
+            // 
+            // labelInfo2
+            // 
+            this.labelInfo2.AutoSize = true;
+            this.labelInfo2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelInfo2.Location = new System.Drawing.Point(12, 108);
+            this.labelInfo2.Name = "labelInfo2";
+            this.labelInfo2.Size = new System.Drawing.Size(194, 25);
+            this.labelInfo2.TabIndex = 4;
+            this.labelInfo2.Text = "Enter new password:";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPassword.Location = new System.Drawing.Point(229, 105);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(384, 30);
+            this.textBoxPassword.TabIndex = 5;
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus.Location = new System.Drawing.Point(12, 211);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(158, 25);
+            this.labelStatus.TabIndex = 6;
+            this.labelStatus.Text = "Status - READY!";
+            // 
+            // textBoxBrittniPassword
+            // 
+            this.textBoxBrittniPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxBrittniPassword.Location = new System.Drawing.Point(229, 141);
+            this.textBoxBrittniPassword.Name = "textBoxBrittniPassword";
+            this.textBoxBrittniPassword.Size = new System.Drawing.Size(384, 30);
+            this.textBoxBrittniPassword.TabIndex = 8;
+            // 
+            // labelInfo3
+            // 
+            this.labelInfo3.AutoSize = true;
+            this.labelInfo3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelInfo3.Location = new System.Drawing.Point(5, 144);
+            this.labelInfo3.Name = "labelInfo3";
+            this.labelInfo3.Size = new System.Drawing.Size(201, 25);
+            this.labelInfo3.TabIndex = 7;
+            this.labelInfo3.Text = "Brittni login password:";
+            // 
+            // progressBarStatus
+            // 
+            this.progressBarStatus.Location = new System.Drawing.Point(17, 239);
+            this.progressBarStatus.Name = "progressBarStatus";
+            this.progressBarStatus.Size = new System.Drawing.Size(731, 23);
+            this.progressBarStatus.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBarStatus.TabIndex = 9;
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(619, 124);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(129, 47);
+            this.buttonStop.TabIndex = 10;
+            this.buttonStop.Text = "Cancel";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 274);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.progressBarStatus);
+            this.Controls.Add(this.textBoxBrittniPassword);
+            this.Controls.Add(this.labelInfo3);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.labelInfo2);
+            this.Controls.Add(this.labelInfo1);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.buttonGo);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Equity Toolbox";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label labelInfo1;
+        private System.Windows.Forms.Label labelInfo2;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.TextBox textBoxBrittniPassword;
+        private System.Windows.Forms.Label labelInfo3;
+        private System.Windows.Forms.ProgressBar progressBarStatus;
+        private System.Windows.Forms.Button buttonStop;
+    }
+}
+
---
+++ Equity Toolbox/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Equity_Toolbox
+{
+    public partial class Form1 : Form
+    {
+        //Global Variables
+        IWebDriver driver;
+        string currentYear = DateTime.Now.Year.ToString();
+
+        //Initialize Form
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        //Form Load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBoxPassword.Text = "Equity" + currentYear + "!";
+            textBoxBrittniPassword.Text = "Equity" + currentYear + "!";
+        }
+
+        public void buttonGo_Click(object sender, EventArgs e)
+        {
+            setup();
+            resetPass();
+            closeDriver();
+            
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            driver.Quit();
+        }
+
+        public void setup()
+        {
+            buttonGo.Enabled = false;
+            labelStatus.Text = "Status - Getting browser ready in background";
+            labelStatus.Refresh();
+            progressBarStatus.Value = 10;
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("headless"); //hide chrome window
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true; //hide cmd prompt window for webdriver
+            driver = new ChromeDriver(service, options);
+            driver.Manage().Cookies.DeleteAllCookies();
+            driver.Manage().Window.Minimize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
+
+            driver.Url = "https://www.equityelevate.com";
+        }
+
+        public void resetPass()
+        {
+            //Login
+            labelStatus.Text = "Status - Logging in to Equity Elevate";
+            labelStatus.Refresh();
+            driver.FindElement(By.Id("Email")).SendKeys("brittnibasso@gmail.com");
+            driver.FindElement(By.Id("Password")).SendKeys(textBoxBrittniPassword.Text);
+            
+            //Select admin user and navigate to roster
+            driver.FindElement(By.ClassName("button-title-h")).Click();
+            driver.FindElement(By.ClassName("button-title-h")).Click();
+            driver.Url = "https://equityelevate.com/home/mybusinessroster";
+
+            //Get name of agent and search for it
+            labelStatus.Text = "Status - Searching for agent..";
+            labelStatus.Refresh();
+            driver.FindElement(By.Name("searchname")).SendKeys(textBoxName.Text);
+            driver.FindElement(By.Id("searchsubmit")).Click();
+            Thread.Sleep(500);
+
+            //Count number of results. If more than 1, stop reset process
+            labelStatus.Text = "Status - Checking if there's more than 1 result";
+            labelStatus.Refresh();
+            Thread.Sleep(750);
+            IList<IWebElement> agents = driver.FindElements(By.XPath("//*[@id=\"exitingform\"]/div/div[2]"));
+            if (agents.Count > 3) 
+            {
+                labelStatus.Text = "Status - More than 1 agent with similar name, proceed manually";
+                labelStatus.Refresh();
+            } //Do nothing if greater than 1. Empty {}'s
+            else
+            {
+                //Select user from list div[1]/div[2] for first user in list or div[2]/div[2] for second user in list
+                labelStatus.Text = "Status - Agent found, starting password reset process";
+                labelStatus.Refresh();
+                driver.FindElement(By.XPath("//*[@id=\"exitingform\"]/div[1]/div[2]/a")).Click();
+                Thread.Sleep(500);
+
+                //Change password button
+                driver.FindElement(By.XPath("//*[@id=\"personal-info-form\"]/div[3]/div/div/div/button")).Click();
+
+                //Enter password, confirm and save
+                driver.FindElement(By.Id("Password1")).SendKeys(textBoxPassword.Text);
+                Thread.Sleep(500);
+                driver.FindElement(By.Id("Password2")).SendKeys(textBoxPassword.Text);
+                driver.FindElement(By.Id("chgPasswordSubmit")).Click();
+                Thread.Sleep(500);
+                driver.FindElement(By.XPath("/html/body/div[2]/div/div[3]/button[1]")).Click();
+                labelStatus.Text = "Status - Password successfully changed";
+                labelStatus.Refresh();
+                Thread.Sleep(500);
+
+            }
+        }
+
+        public void closeDriver()
+        {
+            labelStatus.Text = "Status - Closing connection to browser";
+            labelStatus.Refresh();
+            driver.Quit();
+            Thread.Sleep(1000);
+            labelStatus.Text = "Status - Done";
+            labelStatus.Refresh();
+            buttonGo.Enabled = true;
+            progressBarStatus.Value = 0;
+
+        }
+
+        // On worker thread so do our thing!
+        void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Your background task goes here
+            setup();
+            resetPass();
+            closeDriver();
+            {
+                // Report progress to 'UI' thread
+                //backgroundWorker1.ReportProgress();
+                
+            }
+        }
+        // Back on the 'UI' thread so we can update the progress bar
+        void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // The progress percentage is a property of e
+            progressBarStatus.Value = e.ProgressPercentage;
+        }
+
+
+    }
+    /*    
+     *                //var r = new toolbox();
+     *    public class toolbox
+        {
+            IWebDriver driver;
+
+
+            public void invokeBrowser()
+            {
+                ChromeOptions options = new ChromeOptions();
+                //options.AddArgument("--headless"); //hide chrome window
+                ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+                service.HideCommandPromptWindow = false; //hide cmd prompt window for webdriver
+                driver = new ChromeDriver(service,options);
+                driver.Manage().Cookies.DeleteAllCookies();
+                driver.Manage().Window.Maximize();
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+                driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
+
+                driver.Url = "https://www.equityelevate.com";
+
+            }
+
+            public void resetPass()
+            {
+                //Declarations
+                Form1 f = new Form1();
+                string currentYear = DateTime.Now.Year.ToString();
+                f.label1.Text = currentYear;
+            }
+
+            public void closeBrowser()
+            {
+                driver.Quit();
+            }
+        } */
+
+
+
+}
