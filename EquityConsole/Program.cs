a3464296e31b54bb0bff9b8c9a8def88fd8755fa
Alin Lupascu <lupascu.marius@gmail.com>
2020-05-04 13:46:37 -0600

    Add files via upload

---
+++ EquityConsole/Program.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquityConsole
+{
+    class ResetTool
+    {
+        IWebDriver driver;
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Test done");
+            //Console.ReadLine();
+            var r = new ResetTool();
+            r.invokeBrowser();
+            r.test();
+            r.closeBrowser();
+           
+        }
+
+        void invokeBrowser()
+        {
+            driver = new ChromeDriver();
+        }
+
+        void test()
+        {
+            driver.Url = "http://www.google.com";
+        }
+
+        void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
